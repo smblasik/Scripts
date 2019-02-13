10415a624d96dfd3b7525a6a926a4735e6eb9947
Scott Blasik <smblasik@gmail.com>
2019-02-13 08:19:30 -0600

    Updated install script

--- Setup.sh
+++ Setup.sh
@@ -4,24 +4,24 @@
 sudo pacman -Syu --noconfirm
 
 # Pacman Software to Install
-pacman_list=(
-  terminator
-  zsh
-  python-pip
-  yaourt
-  vim
-  corebird
-  numix-icon-theme-circle
-)
+packages="
+  terminator 
+  zsh 
+  python-pip 
+  yaourt 
+  vim 
+  corebird 
+  numix-icon-theme-circle 
+"
 # Pacman Execution
-sudo pacman -S ${pacman_list[@]} --noconfirm
+sudo pacman -S --noconfirm $packages
 
 # Change Shell to ZSH
 sudo chsh -s `which zsh` $USER
 
 # Enable SSH
-sudo systemctl enable sshd.service
-sudo systemctl start sshd.service
+# sudo systemctl enable sshd.service
+# sudo systemctl start sshd.service
 
 # Data Science
 #pip_list=(
@@ -32,30 +32,18 @@ sudo systemctl start sshd.service
 #  scikit-learn
 #  matplotlib
 #)
-
 #sudo pip install ${pip_list[@]}
 
-# Install atom packages
-atom_list=(
-  emmet
-  file-icons
-  platformio-ide-terminal
-)
-
-apm install ${atom_list[@]} ~/.atom/packages
-
 # Set Yaourt to not prompt
-# echo NOCONFIRM=1 > ~/.yaourtrc
-# echo BUILD_NOCONFIRM=1 >>  ~/.yaourtrc
-# echo EDITFILES=0 >>  ~/.yaourtrc
+echo NOCONFIRM=1 > ~/.yaourtrc
+echo BUILD_NOCONFIRM=1 >>  ~/.yaourtrc
+echo EDITFILES=0 >>  ~/.yaourtrc
 
 # AUR Software to Install
 yaourt_list=(
   google-chrome
-  osx-arc-shadow
+  mojave-gtk-theme-git
   oh-my-zsh-git
-  flat-remix-git
-  flat-remix-gtk-git
 )
 
 # Install AUR packages
@@ -68,10 +56,5 @@ done
 git clone https://github.com/smblasik/Scripts ~/Scripts
 ln ~/Scripts/.zshrc ~/.zshrc
 ln ~/Scripts/.virmc ~/.vimrc
-
-cp ~/Scripts/X-Wing.jpg ~/Pictures/Wallpaper/X-Wing.jpg
-cp ~/Scripts/BlackHat.png ~/Pictures/BlackHat.png
-
-#echo TemperatureFormat=true >> ~/.config/deepin/dcc-weather-plugin.conf
 
 sudo reboot
